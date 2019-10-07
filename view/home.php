ad717f69d5349abb28f5bbc7288b2b514bbf0cb4
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2019-10-07 15:31:02 -0400

    tarlochan

---
+++ view/home.php
@@ -0,0 +1,22 @@
+<div class = "container-fluid">
+    <div class="row">
+    <div class = "col-md-1"></div>
+    <div class = "col-md-10 p-5" >
+        <img src="https://developers.google.com/training/certification/images/GD-Certification-MWS-Cyan.svg" alt="Certification" style="width:100%;height:300px;" alt="Certification">
+        <h1 class="mt-5">How Cert Me Works For You ?</h1>
+        <p>Cert Me Certification lets you demonstrate your proficiency and skill. After you pass a certification exam, you can use your certification to promote yourself to the communities, projects, and employers that are important to you.Cert Me gives both professional and aspiring developers to give exams and helps get Certification easily by following simple easy steps. Take a certification exam to gain recognition for your skills as a developer. When you pass a certification exam, you’ll be featured on the official certification directory. You can include your digital mark in your email signature and embed it on sites such as LinkedIn and Twitter.</p>
+        <h2>Step 1 Registration on Cert Me</h2>
+        <p>The First easy step will be for new users to register an account on Cert Me official website and get yourself set for certification</p>
+        <h2>Step 2 Registration for Certification</h2>
+        <p>The Second Step will be to Look for the certification of keen interest and than do Registration for that certification.</p>
+        <h2>Step 3 Appear For Exam</h2>
+        <p>The Third Step will be to appear for series of exam consiting of Multiple Choice Questions and finish and submit the exam in thr alloted time period.</p>
+    </div>
+    <div class = "col-md-1"></div>
+    </div>
+    <div class="row p-5 bg-primary">
+        <div class="col-md-12 px-5 text-white">
+        <h1>Register Yourself Today -> <a href="/certme/?action=get-access" class="text-white"> Click Here</a></h1>
+        </div>
+    </div>
+</div>
